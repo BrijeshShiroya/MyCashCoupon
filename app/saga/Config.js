684f91af1a2dbce7847207731d3d641e0dd46ff9
BShiroya <brijesh.s@pentair.com>
2021-12-19 13:42:56 +0530

    Added QRcode scanner

--- app/saga/Config.js
+++ app/saga/Config.js
@@ -5,7 +5,7 @@ import { getError } from '../services/Utils';
 export function* getConfig(api) {
   const response = yield call(api.config);
   if (response?.data?.status === 200) {
-    yield put(ConfigActions.configSuccess(response?.data?.data || []));
+    yield put(ConfigActions.configSuccess(response?.data || []));
   } else {
     const error = yield call(getError, response);
     yield put(ConfigActions.configFailure(error));
